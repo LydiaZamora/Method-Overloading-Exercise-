8ce33e521efa2d524469fda7b7a9928e89fbacc0
Lydia Zamora <lydiazamorayoga@gmail.com>
2024-06-19 17:27:18 -0700

    finished exercises

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Console.WriteLine(Add(2, 5));
+
+            // Console.WriteLine(Add(2.1, 3.4));
+
+            Console.WriteLine(Add(3, 1, true));
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int num1, int num2, bool isCheck)
+        {
+            var sum = num1 + num2;
+
+            if (isCheck == true && sum >1)
+            {
+                return $"{sum} dollars";
+            }
+
+            else if(isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+
+            else if(isCheck == true && sum <1)
+            {
+                return $"{sum} dollars";
+            }
+
+            else
+            {
+                return sum.ToString();
+            }
         }
     }
 }
